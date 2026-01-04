023cff31577158af14b738549d2a9e0f6f3d4668
frankiedhon <frankiedhon05@gmail.com>
2026-01-04 22:13:22 +0800

    Add files via upload

---
+++ Exer2_Pelayo/Exer2_Car.java
@@ -0,0 +1,31 @@
+public class Exer2_Car {
+    private String color;
+    private String model;
+    private String brand;
+    private int yearModel;
+
+    // no argument constructor
+    public Exer2_Car() {
+        this.color = "No Color";
+        this.model = "No Model";
+        this.brand = "No Brand";
+        this.yearModel = 0;
+    }
+
+    // parameterized constructor
+    public Exer2_Car(String color, String model, String brand, int yearModel) {
+        this.color = color;
+        this.model = model;
+        this.brand = brand;
+        this.yearModel = yearModel;
+    }
+
+    // methods
+    public void displayInfo() {
+        System.out.println("Color: " + this.color);
+        System.out.println("Model: " + this.model);
+        System.out.println("Brand: " + this.brand);
+        System.out.println("Year Model: " + this.yearModel);
+        System.out.println("----------------------------");
+    }
+}
---
+++ Exer2_Pelayo/Exer2_CarTester.java
@@ -0,0 +1,25 @@
+public class Exer2_CarTester {
+    public static void main(String[] args) {
+        Exer2_Car c1 = new Exer2_Car("White", "Model S", "Tesla", 2022);
+        Exer2_Car c2 = new Exer2_Car("Black", "Challenger", "Dodge", 2021);
+        Exer2_Car c3 = new Exer2_Car("Red", "Supra", "Toyota", 2020);
+        Exer2_Car c4 = new Exer2_Car("Blue", "Wrangler", "Jeep", 2019);
+        Exer2_Car c5 = new Exer2_Car("Silver", "A4", "Audi", 2023);
+        Exer2_Car c6 = new Exer2_Car("Gray", "3 Series", "BMW", 2018);
+        Exer2_Car c7 = new Exer2_Car("Orange", "Elantra", "Hyundai", 2021);
+        Exer2_Car c8 = new Exer2_Car("Green", "Leaf", "Nissan", 2022);
+        Exer2_Car c9 = new Exer2_Car("Yellow", "911", "Porsche", 2023);
+        Exer2_Car c10 = new Exer2_Car("Purple", "C-Class", "Mercedes-Benz", 2020);
+
+        c1.displayInfo();
+        c2.displayInfo();
+        c3.displayInfo();
+        c4.displayInfo();
+        c5.displayInfo();
+        c6.displayInfo();
+        c7.displayInfo();
+        c8.displayInfo();
+        c9.displayInfo();
+        c10.displayInfo();
+    }
+}
